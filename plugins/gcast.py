5f9b7b4791f8ca8ef12a88d5f5ebba065333c401
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2023-04-05 15:03:25 +0530

    Update gcast.py

--- plugins/gcast.py
+++ plugins/gcast.py
@@ -6,8 +6,37 @@ from pyrogram.types import Dialog, Chat, Message
 from pyrogram.errors import UserAlreadyParticipant
 from modules.clientbot.clientbot import client as aditya
 from modules.config import SUDO_USERS
+from modules import bot
 
-@Client.on_message(filters.command(["gcast", "post", "send"]))
+@bot.on_message(filters.command(["gcast", "podcast", "post"]))
+async def broadcast(_, message: Message):
+    sent=0
+    failed=0
+    if message.from_user.id not in SUDO_USERS:
+        return
+    else:
+        wtf = await message.reply("`StarTiNG BroAdCaST...`")
+        if not message.reply_to_message:
+            await wtf.edit("**__Ƥɭɘɑsɘ Ʀɘƥɭy Ƭø ɑ Mɘssɑʛɘ Ƭø Stɑɤt Ɓɤøɑɗƈɑst ...__**")
+            return
+        lmao = message.reply_to_message.text
+        async for dialog in bot.iter_dialogs():
+            try:
+                await bot.send_message(dialog.chat.id, lmao)
+                sent = sent+1
+                await wtf.edit(f"`Ɓɤøɑɗƈɑstɩŋʛ` \n\n**Sɘŋt Ƭø:** `{sent}` Ƈɦɑts \n**Fɑɩɭɘɗ Iŋ:** {failed} chats")
+                await asyncio.sleep(3)
+            except:
+                failed=failed+1
+        await message.reply_text(f"`Ɠƈɑst Sʋƈƈɘssfʋɭɭy` \n\n**Sɘŋt Ƭø:** `{sent}` Ƈɦɑts \n**Fɑɩɭɘɗ Iŋ:** {failed} Ƈɦɑts")
+
+
+
+
+
+#assistant_broadcast
+
+@Client.on_message(filters.command(["agcast", "apost", "asend"]))
 async def broadcast(_, message: Message):
     sent=0
     failed=0
